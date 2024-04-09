221762c3a7e93b6fd136752f669882200f7ad38d
rebeccasun31 <rebeccasun31@gmail.com>
2024-04-10 06:12:15 +0800

    agent1 fin

--- HW2/AI_project2_2024_0408/game_1/Sample.py
+++ HW2/AI_project2_2024_0408/game_1/Sample.py
@@ -4,63 +4,253 @@ import random
 import threading
 
 num_threads = 4
-num_simulations = 10000
+num_simulations = 50
 boardSize = 12
+C_PUCT = 1.5
+# EPS = np.finfo(float).eps
 
 def legalSteps(playerID, mapStat, sheepStat):
     legalSteps = []
-    dirMove = {1:[-1, -1], 2:[0, -1], 3:[1, -1], 4:[-1, 0], 6:[1, 0], 7:[-1, 1], 8:[0, 1], 9:[1, 1]}
+    # dirMove = {1:[-1, -1], 2:[0, -1], 3:[1, -1], 4:[-1, 0], 6:[1, 0], 7:[-1, 1], 8:[0, 1], 9:[1, 1]}
+    dirMove = {1:[-1, -1], 2:[-1, 0], 3:[-1, 1], 4:[0, -1], 6:[0, 1], 7:[1, -1], 8:[1, 0], 9:[1, 1]}
+    
+    cnt = np.sum(mapStat == playerID)
+    if cnt == 0:    # init
+        for i in range(boardSize):
+            for j in range(boardSize):
+                if mapStat[i][j] == 0:
+                    isLegal = False
+                    for dir in [2, 4, 6, 8]:
+                        move = dirMove[dir]
+                        x = j + move[0]
+                        y = i + move[1]
+                        if x < 0 or x == boardSize or y < 0 or y == boardSize:  # border
+                            isLegal = True
+                            break
+                        if x >= 0 and x < boardSize and y >= 0 and y < boardSize and mapStat[y][x] == -1:   # adjacant to -1
+                            isLegal = True
+                            break
+                    if isLegal:
+                        legalSteps.append([i, j])
+        return legalSteps
+
     for i in range(boardSize):
         for j in range(boardSize):
             if mapStat[i][j] == playerID and sheepStat[i][j] > 1:
                 for dir, move in dirMove.items():
-                    x = i + move[0]
-                    y = j + move[1]
-                    if x >= 0 and x < boardSize and y >= 0 and y < boardSize and mapStat[x][y] == 0:
+                    x = j + move[0]
+                    y = i + move[1]
+                    if x >= 0 and x < boardSize and y >= 0 and y < boardSize and mapStat[y][x] == 0:
                         for m in range(1, sheepStat[i][j]):
-                            legalSteps.append([(x, y), m, dir])
+                            legalSteps.append([(i, j), m, dir])
     return legalSteps
 
+def isTerminal(mapStat, sheepStat):
+    for playerID in range(1, 5):
+        if len(legalSteps(playerID, mapStat, sheepStat)) != 0:
+            return False
+    return True
+
+def getValue_dfs(playerID, mapStat, visited, y, x):
+    if y < 0 or y >= boardSize or x < 0 or x >= boardSize or visited[y][x] or mapStat[y][x] != playerID:
+        return 0
+    
+    visited[y][x] = True
+    area = 1
+    
+    area += getValue_dfs(playerID, mapStat, visited, y - 1, x)  # up
+    area += getValue_dfs(playerID, mapStat, visited, y + 1, x)  # down
+    area += getValue_dfs(playerID, mapStat, visited, y, x - 1)  # left
+    area += getValue_dfs(playerID, mapStat, visited, y, x + 1)  # right
+    
+    return area
+
+def getValue(playerID, mapStat):
+    visited = [[False for _ in range(boardSize)] for _ in range(boardSize)]
+    areas = []
+    for y in range(boardSize):
+        for x in range(boardSize):
+            if not visited[y][x] and mapStat[y][x] == playerID:
+                areas.append(getValue_dfs(playerID, mapStat, visited, y, x))
+
+    value = 0
+    for area in areas:
+        value += pow(area, 1.25)
+    value = round(value)
+    
+    return value
+
+def applyStep(playerID, mapStat, sheepStat, step):
+    if len(step) == 2:  #initPos
+        [y, x] = step
+        if mapStat[y][x] != 0 or sheepStat[y][x] != 0: 
+            print("error1")
+            return False
+        mapStat[y][x] = playerID
+        sheepStat[y][x] = 16
+        return True
+
+    [(y, x), m, dir] = step
+    # dirMove = {1:[-1, -1], 2:[0, -1], 3:[1, -1], 4:[-1, 0], 6:[1, 0], 7:[-1, 1], 8:[0, 1], 9:[1, 1]}
+    dirMove = {1:[-1, -1], 2:[-1, 0], 3:[-1, 1], 4:[0, -1], 6:[0, 1], 7:[1, -1], 8:[1, 0], 9:[1, 1]}
+    move = dirMove[dir]
+
+    if m >= sheepStat[y][x] or m <= 0: 
+        print("error2")
+        return False
+    if mapStat[y + move[1]][x + move[0]] != 0: 
+        print("error3")
+        return False
+
+    sheepStat[y][x] -= m
+    while 0 <= y + move[1] < boardSize and 0 <= x + move[0] < boardSize and mapStat[y + move[1]][x + move[0]] == 0:
+        x += move[0]
+        y += move[1]
+
+    mapStat[y][x] = playerID
+    sheepStat[y][x] = m
+
+    return True
+
+
 class Node:
-    def __init__(self):
+    def __init__(self, chosen_step = [], parent = None, playerID = 0):
         self.num_visits = 0
-        self.parent_player_value_sum = 0.0
-        self.current_player_value_sum = 0.0
+        self.value_sum = 0.0
+        self.policy = 0.0
+        self.chosen_step = chosen_step
+        self.parent = parent
         self.children = []
+        self.playerID = playerID
 
 class MCTS:
-    def __init__(self):
+    def __init__(self, playerID, mapStat, sheepStat):
+        self.playerID = playerID
+        self.mapStat = mapStat
+        self.sheepStat = sheepStat
+
         self.threads = []
-        self.roots = list([Node()] * num_threads)
+        self.roots = []
 
-        for i in range(num_threads):
-            thread = threading.Thread(target=self.run, args=(i))
+        for threadID in range(num_threads):
+            self.roots.append(Node())
+            thread = threading.Thread(target=self.run, args=(threadID,))
             self.threads.append(thread)
-
-        for thread in self.threads:
             thread.start()
 
         for thread in self.threads:
             thread.join()
 
     def run(self, threadID):
-        for _ in range(num_simulations):
+        for i in range(num_simulations):
             self.simulation(self.roots[threadID])
 
     def simulation(self, root):
-        self.select()
-        self.evaluate()
-        self.update()
+        leaf_node = root
+        leaf_mapStat = self.mapStat.copy()
+        leaf_sheepStat = self.sheepStat.copy()
 
-    def select(self):
-        pass
+        self.select(root, leaf_node, leaf_mapStat, leaf_sheepStat)
+        
 
-    def evaluate(self):
-        pass
+    def select(self, root, leaf_node, leaf_mapStat, leaf_sheepStat):
+        while len(leaf_node.children) != 0:
+            leaf_node.num_visits += 1
 
-    def update(self):
-        pass
+            best_score = -np.inf
+            selected_node = None
+            for child in leaf_node.children:
+                if child.num_visits == 0:
+                    score = np.inf
+                else:
+                    q = child.value_sum / child.num_visits
+                    u = C_PUCT * np.sqrt(np.log(root.num_visits) / child.num_visits)
+                    score = q + u
 
+                if score > best_score:
+                    best_score = score
+                    selected_node = child
+
+            if selected_node != None:
+                leaf_node = selected_node
+                if not applyStep(leaf_node.playerID, leaf_mapStat, leaf_sheepStat, leaf_node.chosen_step):
+                    print("ERROR: applyStep1")
+                    print(leaf_node.playerID)
+                    print(leaf_mapStat)
+                    print(leaf_sheepStat)
+                    print(leaf_node.chosen_step)
+                    exit()
+                    return "error"
+                
+        leaf_node.num_visits += 1
+
+        if isTerminal(leaf_mapStat, leaf_sheepStat):
+            # leaf_node.policy = leaf_node.num_visits / root.num_visits
+            # leaf_node.value += getValue(self.playerID, leaf_mapStat)
+            self.update(root, leaf_node, leaf_mapStat)
+            return
+
+        # expand
+        legalsteps = legalSteps(leaf_node.playerID % 4 + 1, leaf_mapStat, leaf_sheepStat)
+        for step in legalsteps:
+            leaf_node.children.append(Node(step, leaf_node, leaf_node.playerID % 4 + 1))
+
+        # rollout
+        self.evaluate(root, leaf_node, leaf_mapStat, leaf_sheepStat)
+        return
+
+
+    def evaluate(self, root, leaf_node, leaf_mapStat, leaf_sheepStat):
+        # rollout
+        if len(leaf_node.children) > 0:
+            leaf_node = random.choice(leaf_node.children)
+            leaf_node.num_visits += 1
+
+            if not applyStep(leaf_node.playerID, leaf_mapStat, leaf_sheepStat, leaf_node.chosen_step):
+                print("ERROR: applyStep2")
+                print(leaf_node.playerID)
+                print(leaf_mapStat)
+                print(leaf_sheepStat)
+                print(leaf_node.chosen_step)
+                exit()
+                return "error"
+
+        cur_player = leaf_node.playerID
+        while not isTerminal(leaf_mapStat, leaf_sheepStat):
+            cur_player = cur_player % 4 + 1
+            legalsteps = legalSteps(cur_player, leaf_mapStat, leaf_sheepStat)
+            if len(legalsteps) > 0:
+                step = random.choice(legalsteps)
+                if not applyStep(cur_player, leaf_mapStat, leaf_sheepStat, step):
+                    print("ERROR: applyStep3")
+                    print(cur_player)
+                    print(leaf_mapStat)
+                    print(leaf_sheepStat)
+                    print(legalsteps[0])
+                    exit()
+                    return "error"
+        
+        self.update(root, leaf_node, leaf_mapStat)
+        return
+
+    def update(self, root, leaf_node, leaf_mapStat):
+        while leaf_node.parent != None:
+            leaf_node.policy = leaf_node.num_visits / root.num_visits
+            leaf_node.value_sum += getValue(leaf_node.playerID, leaf_mapStat)
+            leaf_node = leaf_node.parent
+
+        return
+    
+    def getStep(self):
+        all_steps = {}
+        for threadID in range(num_threads):
+            for child in self.roots[threadID].children:
+                if child.num_visits > 0:
+                    step = tuple(child.chosen_step)
+                    all_steps[step] = all_steps.get(step, 0) + child.value_sum / child.num_visits
+
+        bestStep = max(all_steps, key=all_steps.get)
+        return list(bestStep)
 
 
 '''
@@ -72,12 +262,13 @@ class MCTS:
     
 '''
 
-def InitPos(mapStat):
+def InitPos(mapStat, playerID):
     init_pos = [0, 0]
-    '''
-        Write your code here
+    mapStat = mapStat.astype(int)
+    sheepStat = np.zeros((12, 12), dtype=int)
+    mcts = MCTS(playerID, mapStat, sheepStat)
+    init_pos = mcts.getStep()
 
-    '''
     return init_pos
 
 
@@ -101,16 +292,17 @@ def InitPos(mapStat):
 '''
 def GetStep(playerID, mapStat, sheepStat):
     step = [(0, 0), 0, 1]
-    '''
-    Write your code here
-    
-    '''
+    mapStat = mapStat.astype(int)
+    sheepStat = sheepStat.astype(int)
+    mcts = MCTS(playerID, mapStat, sheepStat)
+    step = mcts.getStep()
+
     return step
 
 
 # player initial
 (id_package, playerID, mapStat) = STcpClient.GetMap()
-init_pos = InitPos(mapStat)
+init_pos = InitPos(mapStat, playerID)
 STcpClient.SendInitPos(id_package, init_pos)
 
 # start game
