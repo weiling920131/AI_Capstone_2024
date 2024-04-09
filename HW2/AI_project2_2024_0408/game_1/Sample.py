1f0bca49ba351c25ab841314dbc1073e1c988bc1
weiling920131 <weiling920131@gmail.com>
2024-04-09 17:13:28 +0800

    c++topy

--- HW2/AI_project2_2024_0408/game_1/Sample.py
+++ HW2/AI_project2_2024_0408/game_1/Sample.py
@@ -2,6 +2,23 @@ import STcpClient
 import numpy as np
 import random
 
+boardSize = 12
+
+def legalSteps(playerID, mapStat, sheepStat):
+    legalSteps = []
+    dirMove = {1:[-1, -1], 2:[0, -1], 3:[1, -1], 4:[-1, 0], 6:[1, 0], 7:[-1, 1], 8:[0, 1], 9:[1, 1]}
+    for i in range(boardSize):
+        for j in range(boardSize):
+            if mapStat[i][j] == playerID and sheepStat[i][j] > 1:
+                for dir, move in dirMove.items():
+                    x = i + move[0]
+                    y = j + move[1]
+                    if x >= 0 and x < boardSize and y >= 0 and y < boardSize and mapStat[x][y] == 0:
+                        for m in range(1, sheepStat[i][j]):
+                            legalSteps.append([(x, y), m, dir])
+    return legalSteps
+
+
 '''
     選擇起始位置
     選擇範圍僅限場地邊緣(至少一個方向為牆)
@@ -10,7 +27,6 @@ import random
     init_pos=[x,y],代表起始位置
     
 '''
-
 
 def InitPos(mapStat):
     init_pos = [0, 0]
